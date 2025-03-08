f8c77e1996df4273d5f44f2927357561fa7595c5
Steven de Salas <sdesalas@users.noreply.github.com>
2025-03-08 23:05:06 +0100

    Update WiegandMultiReader.h

--- WiegandMultiReader.h
+++ WiegandMultiReader.h
@@ -1,3 +1,15 @@
+/**
+ * WiegandMultiReader
+ * 
+ * By Steven de Salas
+ * 
+ * Upstream source code written by JP Liew
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ */
+
 #ifndef _WIEGAND_MULTI_H_
 #define _WIEGAND_MULTI_H_
 
