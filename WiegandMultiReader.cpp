c1568338b60214355eebff92cdbd0191f96a1542
Steven de Salas <sdesalas@users.noreply.github.com>
2025-03-08 23:04:54 +0100

    Update WiegandMultiReader.cpp

--- WiegandMultiReader.cpp
+++ WiegandMultiReader.cpp
@@ -1,6 +1,18 @@
+/**
+ * WiegandMultiReader
+ * 
+ * By Steven de Salas
+ * 
+ * Upstream source code written by JP Liew
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ */
 #include "WiegandMultiReader.h"
 
 volatile unsigned long _cardTemp=0;
+volatile unsigned long _cardTempHigh=0;
 volatile unsigned long _lastWiegand=0;
 unsigned long _code=0;
 volatile int _bitCount=0;	
